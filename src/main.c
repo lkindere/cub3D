0fa83ba30e3644b09124461d6a507cc596b25290
Max Meisinger <mmeising@student.42heilbronn.de>
2022-06-16 06:55:22 -0700

    removed do_rays from main

--- src/main.c
+++ src/main.c
@@ -6,43 +6,11 @@
 /*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/11 23:15:33 by mmeising          #+#    #+#             */
-/*   Updated: 2022/06/16 06:46:24 by mmeising         ###   ########.fr       */
+/*   Updated: 2022/06/16 06:48:40 by mmeising         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
-
-// data->d_x = cos(data->angle);
-// data->d_y = sin(data->angle);
-void	do_rays(t_data *data)
-{
-	float	x;
-	float	y;
-	float	angle;
-	float	d_x;
-	float	d_y;
-
-	angle = data->angle - M_PI_4;
-	ft_memset(data->rays->pixels, 0,
-		data->map_->width * data->ts * data->map_->height * data->ts * sizeof(int));
-	while (angle < data->angle + M_PI_4)
-	{
-		d_x = cos(angle);
-		d_y = sin(angle);
-		x = data->p_x;
-		y = data->p_y;
-		for (float i = 0; i < 100; i += 0.01)
-		{
-			if (!((x > 0 && x < data->map_->width * data->ts)
-				&& (y > 0 && y < data->map_->height * data->ts)))
-				break ;
-			mlx_put_pixel(data->rays, x, y, 0x00FF00FF);
-			x = x + d_x;
-			y = y + d_y;
-		}
-		angle += 0.006544984694979;
-	}
-}
 
 void	put_walls(t_data *data, char **map)
 {
