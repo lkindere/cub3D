b12b1c1e995b40fa8878f72f58749c88d16356b1
Lukas Kinderevicius <lkindere@student.42heilbronn.de>
2022-06-16 14:03:02 +0200

    Checking .cub extension, header changes

---
+++ include/map.h
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkindere <lkindere@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 12:19:30 by lkindere          #+#    #+#             */
+/*   Updated: 2022/06/16 12:28:49 by lkindere         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAP_H
+# define MAP_H
+
+typedef enum e_errors
+{
+	VALID,
+	INVALID_ARGS,
+	INVALID_MAP_FORMAT,
+	INVALID_PATH_FORMAT,
+	INVALID_COLOR_FORMAT,
+	DUPLICATE_DEFINITION,
+	UNCLOSED_MAP,
+	NO_MAP,
+	MALLOC
+}	t_errors;
+
+typedef enum e_pos
+{
+	E = 0,
+	S = 1,
+	W = 2,
+	N = 3,
+	NO_POS
+}	t_pos;
+
+typedef struct s_map
+{
+	char			**map;
+	char			*no;
+	char			*so;
+	char			*we;
+	char			*ea;
+	int				floor_color;
+	int				ceiling_color;
+	unsigned int	width;
+	unsigned int	height;
+	unsigned int	pos_x;
+	unsigned int	pos_y;
+	enum e_pos		position;
+	enum e_errors	valid;
+}	t_map;
+
+#endif
--- include/parser.h
+++ include/parser.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mmeising <mmeising@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lkindere <lkindere@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/12 16:34:24 by lkindere          #+#    #+#             */
-/*   Updated: 2022/06/15 00:22:04 by mmeising         ###   ########.fr       */
+/*   Updated: 2022/06/16 12:38:47 by lkindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,46 +20,7 @@
 # include <fcntl.h>
 # include <sys/errno.h>
 
-# include <stdio.h>
-
-typedef enum e_errors
-{
-	VALID,
-	INVALID_ARGS,
-	INVALID_MAP_FORMAT,
-	INVALID_PATH_FORMAT,
-	INVALID_COLOR_FORMAT,
-	DUPLICATE_DEFINITION,
-	UNCLOSED_MAP,
-	NO_MAP,
-	MALLOC
-}	t_errors;
-
-typedef enum e_pos
-{
-	E = 0,
-	S = 1,
-	W = 2,
-	N = 3,
-	NO_POS
-}	t_pos;
-
-typedef struct s_map
-{
-	char			**map;
-	char			*no;
-	char			*so;
-	char			*we;
-	char			*ea;
-	int				floor_color;
-	int				ceiling_color;
-	unsigned int	width;
-	unsigned int	height;
-	unsigned int	pos_x;
-	unsigned int	pos_y;
-	enum e_pos		position;
-	enum e_errors	valid;
-}	t_map;
+# include "map.h"
 
 int		parser(int argc, char **argv, t_map *map);
 
