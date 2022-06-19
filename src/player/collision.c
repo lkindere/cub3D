48eda85cd573b581a5372601e62fb67535a86a8c
Lukas Kinderevicius <lkindere@student.42heilbronn.de>
2022-06-20 01:53:59 +0200

    Small change

--- src/player/collision.c
+++ src/player/collision.c
@@ -6,7 +6,7 @@
 /*   By: lkindere <lkindere@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 05:49:20 by lkindere          #+#    #+#             */
-/*   Updated: 2022/06/20 01:44:28 by lkindere         ###   ########.fr       */
+/*   Updated: 2022/06/20 01:50:03 by lkindere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,14 +39,17 @@ int	check_collision(t_data *data, float x, float y)
 {
 	x = x * data->speed * (data->mlx->delta_time * 60);
 	y = y * data->speed * (data->mlx->delta_time * 60);
-	while (y < 0 && is_wall(data, 0, y, data->map))
-		y += 0.015625;
-	while (y > 0 && is_wall(data, 0, y, data->map))
-		y -= 0.015625;
-	while (x < 0 && is_wall(data, x, y, data->map))
-		x += 0.015625;
-	while (x > 0 && is_wall(data, x, y, data->map))
-		x -= 0.015625;
+	if (is_wall(data, x, y, data->map))
+	{
+		while (y < 0 && is_wall(data, 0, y, data->map))
+			y += 0.015625;
+		while (y > 0 && is_wall(data, 0, y, data->map))
+			y -= 0.015625;
+		while (x < 0 && is_wall(data, x, y, data->map))
+			x += 0.015625;
+		while (x > 0 && is_wall(data, x, y, data->map))
+			x -= 0.015625;
+	}
 	data->p_x += x;
 	data->p_y += y;
 	return (0);
